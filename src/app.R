a60d7ebdb90ee5aac7fb1609327171d04c524c74
Rob Blumberg <robertblumberg97@gmail.com>
2019-12-13 16:55:28 -0800

    Made formatting changes

--- src/app.R
+++ src/app.R
@@ -38,8 +38,8 @@ graph3 <- dccGraph(
 app$layout(
   htmlDiv(
     list(
-      htmlH1('Fate of Titanic Passengers by Location on Ship'),
-      htmlH2('Description'),
+      htmlImg(src='https://github.com/RobBlumberg/titanic_image/blob/master/titanic_header_new.png?raw=true', width='1780px'),
+      htmlH4('The sinking of the Titanic on April 12, 1912 four days into its maiden voyage as the world’s largest ocean liner is one of the worst maritime disasters of all time which killed over 1,500 of the estimated 2,224 passengers and crew on board. This app has been created to support an exploratory research proposal on the correlation of passenger location with survival rates which may be used to improve and democratize safety in the design of large passenger ships.'),
       #selection components go here
       htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
       htmlLabel('Select deck level'),
